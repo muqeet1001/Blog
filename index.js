2c9e7a93695060028241e86cff8d1d2dbe707a33
muqeet1001 <abdul1002muqeet@gmail.com>
2025-11-13 22:26:05 +0530

    setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;  
+
+app.set('view engine', 'ejs');
+app.set('views',path.resolve('./src/views'))
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
